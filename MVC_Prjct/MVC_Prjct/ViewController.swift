e5f2fcff4d5bca4cf185dd967e7c2161c70ded14
skyworkerxsky <pwuser@yandex.ru>
2020-01-15 00:46:44 +0500

    init

---
+++ MVC_Prjct/MVC_Prjct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC_Prjct
+//
+//  Created by Алексей Макаров on 15.01.2020.
+//  Copyright © 2020 Алексей Макаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
